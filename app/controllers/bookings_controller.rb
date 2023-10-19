9f6ca482caa4522ac1264400aeb4e0eccfc37e7c
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-10-19 20:04:23 +0400

    add form and form validation

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,18 @@
+class BookingsController < ApplicationController
+  def create
+    @booking = Booking.new bookings_params
+
+    if @booking.save
+      flash[:success] = 'Booking created'
+      redirect_to root_path
+    else
+      render 'home/index', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def bookings_params
+    params.require(:booking).permit(:pick_up, :drop_off, :booking_date, :booking_time)
+  end
+end
